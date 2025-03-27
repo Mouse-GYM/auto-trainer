13f7eb8cb4d6089ac4202d29dbe0a651c32e0dd8
Kevin DeVries <kevin@leaflabs.com>
2025-03-27 13:54:21 -0400

    supporing files

---
+++ auto-trainer-core/src/autotrainer/core/message/motor_configuration_message.py
@@ -0,0 +1,139 @@
+from typing import Protocol, Tuple
+from enum import IntEnum
+
+"""
+Identifiers for the different motor types
+"""
+
+
+class Motor(IntEnum):
+    NONE = 0
+    MAGNET_SERVO = 1
+    PELLET_X_MOTOR = 2
+    PELLET_Y_MOTOR = 3
+    PELLET_Z_MOTOR = 4
+    PELLET_COVER_SERVO = 5
+    PELLET_LOAD_SERVO = 6
+
+
+"""
+Protocol for the interface for obtaining stepper motor configuration entities
+"""
+
+
+class StepperConfigMessage(Protocol):
+    """
+    Motor associated with the configuration
+    """
+
+    @property
+    def motor(self) -> Motor: ...
+
+    """
+    Units: turns/sec
+    """
+
+    @property
+    def maximum_velocity(self) -> float: ...
+
+    """
+    Units: turns/sec^2
+    """
+
+    @property
+    def maximum_acceleration(self) -> float: ...
+
+    """
+    Units: 1/min step
+    """
+
+    @property
+    def minimum_step_inverted(self) -> int: ...
+
+    """
+    Units: steps/rev
+    """
+
+    @property
+    def steps_per_revolution(self) -> float: ...
+
+
+"""
+Protocol for the interface for obtaining servo motor configuration entities
+"""
+
+
+class ServoConfigMessage(Protocol):
+    """
+    Motor associated with the configuration
+    """
+
+    @property
+    def motor(self) -> Motor: ...
+
+    """
+    Units: deg/sec
+    """
+
+    @property
+    def maximum_velocity(self) -> float: ...
+
+    """
+    Units: deg/sec^2
+    """
+
+    @property
+    def maximum_acceleration(self) -> float: ...
+
+    """
+    Units: deg
+    """
+
+    @property
+    def minimum_position(self) -> float: ...
+
+    """
+    Units: deg
+    """
+
+    @property
+    def maximum_position(self) -> float: ...
+
+    """
+    Units: usec
+    """
+
+    @property
+    def min_pwm_duration(self) -> float: ...
+
+    """
+    Units: usec
+    """
+
+    @property
+    def max_pwm_duration(self) -> float: ...
+
+
+"""
+Protocol for access to a full set of motor configurations
+"""
+
+
+class MotorConfigurations(Protocol):
+    @property
+    def magnet_config(self) -> Tuple[Motor, ServoConfigMessage]: ...
+
+    @property
+    def load_config(self) -> Tuple[Motor, ServoConfigMessage]: ...
+
+    @property
+    def cover_config(self) -> Tuple[Motor, ServoConfigMessage]: ...
+
+    @property
+    def x_config(self) -> Tuple[Motor, StepperConfigMessage]: ...
+
+    @property
+    def y_config(self) -> Tuple[Motor, StepperConfigMessage]: ...
+
+    @property
+    def z_config(self) -> Tuple[Motor, StepperConfigMessage]: ...
---
+++ auto-trainer-device/src/autotrainer/device/compound_movement_file.py
@@ -0,0 +1,65 @@
+"""
+Class to manage compound movement configuration file.
+"""
+
+import logging
+import yaml
+from pathlib import Path
+
+from .motor_steps import MotorSteps
+
+logger = logging.getLogger(__name__)
+
+
+class CompoundMovementFile:
+
+    def __init__(self, filename):
+        if isinstance(filename, str):
+            filename = Path(filename)
+
+        logger.info(f"LOADING: Alogus Compound Movement file: {filename}")
+
+        load_movement = MotorSteps("load", [])
+        home_movement = MotorSteps("home", [])
+        send_movement = MotorSteps("send", [])
+
+        if filename.exists():
+            try:
+                with open(filename, "r") as file:
+                    conf = yaml.safe_load(file)
+
+                    if "actions" in conf:
+                        if "load" in conf["actions"]:
+                            load_movement = MotorSteps.from_dict("load",
+                                                                 conf["actions"]["load"])
+                        if "home" in conf["actions"]:
+                            home_movement = MotorSteps.from_dict("home",
+                                                                 conf["actions"]["home"])
+                        if "send" in conf["actions"]:
+                            send_movement = MotorSteps.from_dict("send",
+                                                                 conf["actions"]["send"])
+
+                logging.info("LOADED: Alogus Compound Movement file")
+
+            except Exception as e:
+                logger.error(f"ERROR: Alogus Compound Movement file {filename}: {e}")
+
+        self._load_movement = load_movement
+        self._home_movement = home_movement
+        self._send_movement = send_movement
+
+    '''
+    Meet the CompoundMovementDataSet Protocol
+    '''
+
+    @property
+    def load(self) -> MotorSteps:
+        return self._load_movement
+
+    @property
+    def home(self) -> MotorSteps:
+        return self._home_movement
+
+    @property
+    def send(self) -> MotorSteps:
+        return self._send_movement
