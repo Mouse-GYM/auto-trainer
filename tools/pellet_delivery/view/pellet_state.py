afcec21ff10bfdd412d9a74912e216c9882cbe47
Kevin DeVries <kevin@leaflabs.com>
2025-04-16 15:19:50 -0400

    Using led's instead of checkbox for status.

--- tools/pellet_delivery/view/pellet_state.py
+++ tools/pellet_delivery/view/pellet_state.py
@@ -1,18 +1,42 @@
-from PySide6.QtWidgets import QWidget, QGridLayout, QHBoxLayout, \
-    QCheckBox, QFormLayout
+from PySide6.QtWidgets import QWidget, QHBoxLayout, QFormLayout, QLabel
 
 from tools.pellet_delivery.model.app_model import AppModel
 from tools.view.basic_panel import create_panel
 
+import qtawesome as qta
+
+
+class Led(QLabel):
+    LED_SIZE = 18
+
+    def __init__(self, use_red: bool = False, parent=None):
+        super(Led, self).__init__(parent)
+
+        self._use_red = use_red
+        self.setFixedSize(Led.LED_SIZE, Led.LED_SIZE)
+        self.green_led = qta.icon('fa5s.life-ring', color='green')
+        self.red_led = qta.icon('fa5s.life-ring', color='red')
+        self.gray_led = qta.icon('fa5s.life-ring', color='gray')
+
+        self.off()
+
+    def on(self):
+        self.setPixmap(self.green_led.pixmap(Led.LED_SIZE, Led.LED_SIZE))
+
+    def off(self):
+        led = self.red_led if self._use_red else self.gray_led
+        self.setPixmap(led.pixmap(Led.LED_SIZE, Led.LED_SIZE))
+
+    def set_light(self, setting: bool):
+        self.on() if setting else self.off()
+
 
 def _create_door_panel():
     layout = QFormLayout()
-    front_door = QCheckBox()
-    front_door.nextCheckState = lambda: None
+    front_door = Led()
     layout.addRow("Front Door:", front_door)
 
-    drawer_door = QCheckBox()
-    drawer_door.nextCheckState = lambda: None
+    drawer_door = Led()
     layout.addRow("Drawer Door:", drawer_door)
 
     return front_door, drawer_door, create_panel("Doors", layout)
@@ -22,8 +46,7 @@ def _create_stimulus_panel():
     layout = QFormLayout()
     inputs = []
     for i in range(4):
-        box = QCheckBox()
-        box.nextCheckState = lambda: None
+        box = Led()
         inputs.append(box)
         layout.addRow("Stimulus #" + str(i + 1) + ':', box)
 
@@ -52,12 +75,12 @@ class PelletState(QWidget):
     def _model_property_changed(self, name: str, value, _old_value):
         if name == "is_connected":
             if not value:
-                self._front_door.setChecked(False)
-                self._drawer_door.setChecked(False)
+                self._front_door.set_light(False)
+                self._drawer_door.set_light(False)
         elif name == "front_door":
-            self._front_door.setChecked(bool(value))
+            self._front_door.set_light(bool(value))
         elif name == "drawer_door":
-            self._drawer_door.setChecked(bool(value))
+            self._drawer_door.set_light(bool(value))
         elif name == "stimuli":
             for v, box in zip(value, self._stimulus):
-                box.setChecked(bool(v))
+                box.set_light(bool(v))
