c55c1966672078371536603b2fdeffe8212de7d5
Grégory Starck <gregory.starck@toptal.com>
2025-06-13 13:54:45 -0400

    Enh: make HeadFixMeasurement a dataclass

--- auto-trainer-core/src/autotrainer/core/analysis/head_fix_measurement.py
+++ auto-trainer-core/src/autotrainer/core/analysis/head_fix_measurement.py
@@ -1,6 +1,8 @@
-import typing
+import dataclasses
+from typing import List
 
 
+@dataclasses.dataclass
 class HeadFixMeasurement:
     when: float = 0
     timestamp: int = 0
@@ -9,18 +11,5 @@ class HeadFixMeasurement:
     pressure: float = 0
     temperature: float = 0
     humidity: float = 0
-    spectrum: typing.List[float] = []
+    spectrum: List[float] = dataclasses.field(default_factory=list)
     head_contact: bool = False
-
-    def __init__(self, when: float = 0, timestamp: int = 0, weight: float = 0, switch: float = 0,
-                 pressure: float = 0,
-                 temperature: float = 0, humidity: float = 0,
-                 spectrum: typing.Optional[typing.List[float]] = None):
-        self.when = when
-        self.timestamp = timestamp
-        self.weight = weight
-        self.switch = switch
-        self.pressure = pressure
-        self.temperature = temperature
-        self.humidity = humidity
-        self.spectrum = spectrum if spectrum is not None else []
