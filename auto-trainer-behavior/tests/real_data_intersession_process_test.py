f64a3654be25a745fb20df310c08cb5ef7bb57c0
Grégory Starck <gregory.starck@toptal.com>
2025-06-25 14:32:37 -0400

    Auto-correct expected frame count for identify_dropped_frames (#162)
    * Use only round to get expected frame count from timestamp files + auto-correct if need
    
    * Add test for fp-and-xp are not same
    
    * Enable LFS
    
    * Missed .gitattributes
    
    * Add needed calib files
    
    * Put .gitattributes files top
    
    * Enh/Fix: return the raw str object/value for SceneElements.
    
    So to not require pickle when used as columns index for df saved into hd5 files.
    
    * Remove unnecessary files for the test case

---
+++ auto-trainer-behavior/tests/real_data_intersession_process_test.py
@@ -0,0 +1,26 @@
+from datetime import datetime
+from pathlib import Path
+
+import verboselogs
+
+from autotrainer.behavior import intersession_process
+from autotrainer.behavior.analysis import IntersessionResponse
+
+this_dir = Path(__file__).parent.resolve()
+
+
+def test_fp_and_xp_not_same(project_info, caplog):
+    project_info.session.value = 2
+    project_info.root = this_dir.joinpath("fp-and-xp-not-same").as_posix()
+    project_info.device_id = "agx001"
+    project_info.when = datetime(2025, 6, 23)
+    caplog.set_level(verboselogs.VERBOSE)
+    res = intersession_process(
+        project_info,
+        calib_dir=this_dir.joinpath("4mm_6r_8c_4x"),
+    )
+    assert "Correcting expected_frame_count from " in caplog.text
+    assert isinstance(res, IntersessionResponse)
+    assert res.food_consumed == 0
+    assert res.pellets_presented == 0
+    assert res.successful_reaches == 0
