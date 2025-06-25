d9913fa610e54191bf64d1dd71af75b5a2efb4a0
Grégory Starck <gregory.starck@toptal.com>
2025-06-25 13:04:37 -0400

    Enh/Fix: return the raw str object/value for SceneElements. (#165)
    So to not require pickle when used as columns index for df saved into hd5 files.

--- auto-trainer-inference/src/autotrainer/inference/__init__.py
+++ auto-trainer-inference/src/autotrainer/inference/__init__.py
@@ -1,3 +1,10 @@
+# NB: this is to be back-compatible with h5 files possibly saved using pose_elements when they were defined
+# in this sub-package (as autotrainer.inference.pose_elements)
+from autotrainer.core import pose_elements
+import sys
+sys.modules[f"{__name__}.pose_elements"] = pose_elements
+# this is/should be temporary. TODO: remove some when later
+
 from .pose_algorithm import PoseAlgorithm, PoseResponse, PoseLocation, PoseTuple
 from .pose_model import PoseModel
 from .pose_process import PoseProcess, InferenceCommandMessageKind, InferenceStatusMessageKind, InferenceMode
