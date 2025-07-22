f402fa503d56931b1f9d83454e1ebf3f7007fb67
Grégory Starck <gregory.starck@toptal.com>
2025-07-22 17:10:25 -0400

    Reduce debug log recurrence

--- tools/acquisition/model/inference_model.py
+++ tools/acquisition/model/inference_model.py
@@ -484,7 +484,7 @@ class InferenceModel(ObservableObject, InferenceProtocol, ProjectDependentProtol
 
             t_now = time.time()
             if t_now > t_log_counters:
-                t_log_counters = t_now + 5
+                t_log_counters = t_now + 30
                 if logger.isEnabledFor(logging.DEBUG):
                     logger.debug("status=%s data=%s avg_writes_h5_live=%.6f skipped_h5_live=%s",
                                 self._status, cnt_data_received,
