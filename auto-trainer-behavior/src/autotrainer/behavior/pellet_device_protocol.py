5263e34b08b097b4418d2a87324cc1d6072e8e8f
Patrick Edson <pedson@users.noreply.github.com>
2025-07-25 09:21:59 -0400

    Update saving of animal metadata with new hardware->message handler behavior.

--- auto-trainer-behavior/src/autotrainer/behavior/pellet_device_protocol.py
+++ auto-trainer-behavior/src/autotrainer/behavior/pellet_device_protocol.py
@@ -11,27 +11,55 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def set_x(self, value: int, *, absolute: bool = True) -> Optional[UUID]: ...
+
     """
-    Request a move for the X stepper.
+    Change the X stepper location and set it as the X-axis pellet release location.
     
     :return: A token to expect from the device message handler when the request is complete.
     """
 
     def set_y(self, value: int, *, absolute: bool = True) -> Optional[UUID]: ...
+
     """
-    Request a move for the Y stepper.
+    Change the Y stepper location and set it as the Y-axis pellet release location.
     
     :return: A token to expect from the device message handler when the request is complete.
     """
 
     def set_z(self, value: int, *, absolute: bool = True) -> Optional[UUID]: ...
+
     """
-    Request a move for the Z stepper.
+    Change the Z stepper location and set it as the Z-axis pellet release location.
     
     :return: A token to expect from the device message handler when the request is complete.
     """
 
+    def move_x(self, value: int, *, absolute: bool = True) -> Optional[UUID]: ...
+
+    """
+    Move the X stepper.  This may not be supported on all device platforms.
+
+    :return: A token to expect from the device message handler when the request is complete.
+    """
+
+    def move_y(self, value: int, *, absolute: bool = True) -> Optional[UUID]: ...
+
+    """
+    Move the Y stepper.  This may not be supported on all device platforms.
+
+    :return: A token to expect from the device message handler when the request is complete.
+    """
+
+    def move_z(self, value: int, *, absolute: bool = True) -> Optional[UUID]: ...
+
+    """
+    Move the Z stepper.  This may not be supported on all device platforms.
+
+    :return: A token to expect from the device message handler when the request is complete.
+    """
+
     def send_home(self) -> Optional[UUID]: ...
+
     """
     Request a move to 0, 0, 0.
     
@@ -39,6 +67,7 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def load_pellet(self) -> Optional[UUID]: ...
+
     """
     Request a full load cycle to scoop the pellet from the bin.
     
@@ -46,6 +75,7 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def send_pellet(self) -> Optional[UUID]: ...
+
     """
     Request a move from the current position to the "send" location stored in the hardware.
     
@@ -53,6 +83,7 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def cover_pellet(self) -> Optional[UUID]: ...
+
     """
     Request the barrier arm close and cover the pellet.
     
@@ -60,6 +91,7 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def release_pellet(self) -> Optional[UUID]: ...
+
     """
     Request the barrier arm open and expose the pellet.
     
@@ -67,6 +99,7 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def play_tone(self, frequency: int, duration: float) -> Optional[UUID]: ...
+
     """
     Request the device play a tone.
     
@@ -74,4 +107,5 @@ class PelletDeviceProtocol(ObservableObjectProtocol, Protocol):
     """
 
     def set_motor_drift(self, drift: Offset3DTuple): ...
+
     """Set the motor drift offset"""
