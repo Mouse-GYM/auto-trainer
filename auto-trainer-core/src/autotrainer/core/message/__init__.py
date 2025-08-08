8c032efa335602cef30cbb41d1202edb17b771e7
Grégory Starck <gregory.starck@toptal.com>
2025-08-08 16:38:21 -0400

    SystemDataArgsKwargs: init like this

--- auto-trainer-core/src/autotrainer/core/message/__init__.py
+++ auto-trainer-core/src/autotrainer/core/message/__init__.py
@@ -7,7 +7,9 @@ class SystemDataArgsKwargs:
     """A dedicated dataclass to pass data any desired args and/or kwargs as a single value for system commands"""
 
     def __init__(self, *args, **kwargs):
-        super().__init__(args=args, kwargs=kwargs)
+        # super().__init__(args=args, kwargs=kwargs)
+        self.args = args
+        self.kwargs = kwargs
 
     args: Tuple[Any] = ()
     kwargs: Dict[str, Any] = dataclasses.field(default_factory=dict)
