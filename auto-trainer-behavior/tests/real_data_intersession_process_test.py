7f54164f8b1939cba1adf64d071f36691e0ad756
Patrick Edson <pedson@users.noreply.github.com>
2025-08-14 10:06:22 -0400

    Some platform file-extension assumptions that are not worth fixing for other platforms as this time. (#202)
    * Some platform file-extension assumptions that are not worth fixing for other platforms as this time.
    
    * Update auto-trainer-behavior/tests/real_data_intersession_process_test.py
    
    Co-authored-by: Grégory Starck <gregory.starck@toptal.com>
    
    ---------
    
    Co-authored-by: Grégory Starck <gregory.starck@toptal.com>

--- auto-trainer-behavior/tests/real_data_intersession_process_test.py
+++ auto-trainer-behavior/tests/real_data_intersession_process_test.py
@@ -1,3 +1,4 @@
+import os
 from datetime import datetime
 from pathlib import Path
 
@@ -9,7 +10,11 @@ from autotrainer.behavior.analysis import IntersessionResponse
 this_dir = Path(__file__).parent.resolve()
 
 
+import pytest
+
+@pytest.mark.skipif(os.name != "posix", reason="disabled on non-posix")
 def test_fp_and_xp_not_same(project_info, caplog):
+
     project_info.session.value = 2
     project_info.root = this_dir.joinpath("fp-and-xp-not-same").as_posix()
     project_info.device_id = "agx001"
