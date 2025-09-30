f9d3e264bb6de4a88a7f6ec5f441b13088aa8410
Grégory Starck <gregory.starck@toptal.com>
2025-09-30 08:42:42 -0400

    Reduce deciamls to 1 for pellet deliver xyz control

--- tools/pellet_delivery/view/pellet_control.py
+++ tools/pellet_delivery/view/pellet_control.py
@@ -39,7 +39,7 @@ def add_position(label: str, s_min: float, s_max: float) -> Tuple[
     pos = QDoubleSpinBox()
     pos.setMinimumWidth(40)
     pos.setRange(s_min, s_max)
-    pos.setDecimals(2)
+    pos.setDecimals(1)
     pos.setSingleStep(0.15)
     pos.setWrapping(False)
     position_layout.addWidget(pos, 0)
