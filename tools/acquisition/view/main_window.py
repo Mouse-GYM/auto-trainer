14b75099a41b382bde50e559ecdea66a7d4f22d8
Grégory Starck <gregory.starck@toptal.com>
2025-10-15 09:07:20 -0400

    Add source to emergency top bar status

--- tools/acquisition/view/main_window.py
+++ tools/acquisition/view/main_window.py
@@ -462,9 +462,9 @@ class MainWindow(QMainWindow):
         emergency_button.setObjectName("EmergencyButton")
         emergency_button.setStyleSheet("#EmergencyButton {background-color: red; color: white; min-width: 100px}")
 
-        def update_emergency_ui(is_toggled):
+        def update_emergency_ui(is_toggled: bool, source: str):
             emergency_button.setText("Resume" if is_toggled else "Emergency Stop")
-            self.setWindowTitle(f"{self._title} - BEHAVIOR ALGORITHM PAUSED" if is_toggled else self._title)
+            self.setWindowTitle(f"{self._title} - BEHAVIOR ALGORITHM PAUSED - Source: {source}" if is_toggled else self._title)
             emergency_button.setChecked(is_toggled)
 
         def emergency_stop_triggered(is_toggled: bool):
@@ -472,8 +472,8 @@ class MainWindow(QMainWindow):
             (behavior.emergency_stop if is_toggled else behavior.emergency_resume)("user-button")
 
         emergency_button.toggled.connect(emergency_stop_triggered)
-        behavior.emergency_stopped += lambda src: update_emergency_ui(True)
-        behavior.emergency_resumed += lambda src: update_emergency_ui(False)
+        behavior.emergency_stopped += lambda src: update_emergency_ui(True, source=src)
+        behavior.emergency_resumed += lambda src: update_emergency_ui(False, source=src)
 
         toolbar.addWidget(emergency_button)
 
