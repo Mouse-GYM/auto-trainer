d868cafbbfd03898f147f7f061b34413fa178f4b
Grégory Starck <gregory.starck@toptal.com>
2025-10-29 09:48:40 -0400

    Allow run inference_process from CLI

--- auto-trainer-inference/src/autotrainer/inference/analysis/__main__.py
+++ auto-trainer-inference/src/autotrainer/inference/analysis/__main__.py
@@ -1,5 +1,9 @@
 import argparse
+import logging
 import sys
+from pathlib import Path
+from pprint import pprint
+from datetime import datetime, date
 
 import verboselogs
 
@@ -13,15 +17,24 @@ logger = get_verbose_logger(__name__)
 
 def main():
     parser = argparse.ArgumentParser()
-    parser.add_argument()
-    # TODO: add arguments to allow pass needed args to reconstitute an ProjectInfo full instance,
+    parser.add_argument("unit")
+    parser.add_argument("date", type=lambda v: datetime.strptime(v, "%Y%m%d"))
+    parser.add_argument("session", type=int)
+    parser.add_argument("--data-dir", type=Path, required=True)
+    parser.add_argument("--calib-dir", type=Path, required=True)
     args = parser.parse_args()
     # and then execute:
-    project_info = ProjectInfo(**vars(args))  # smth like that
-    result = intersession_process(project_info)
-    print(f"{project_info.get_session_path()} => {result}")
+    project_info = ProjectInfo(
+        root=args.data_dir,
+        device_id=args.unit,
+        when=args.date,
+        session=args.session,
+    )
+    print(project_info.get_session_path())
+    result = intersession_process(project_info, calib_dir=args.calib_dir)
+    pprint(result)
 
 
 if __name__ == "__main__":
-    setup_logging("autotrainer", logger_level=verboselogs.VERBOSE)
+    setup_logging("autotrainer", logger_level=logging.DEBUG)
     sys.exit(main())
