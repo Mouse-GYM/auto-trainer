343fc0b30054947c9f35c9ac472000850734f984
Grégory Starck <gregory.starck@toptal.com>
2025-12-11 17:28:09 -0500

    slightly better

--- auto-trainer-behavior/src/autotrainer/behavior/pellet/pellet_machine.py
+++ auto-trainer-behavior/src/autotrainer/behavior/pellet/pellet_machine.py
@@ -358,7 +358,8 @@ class PelletMachine(StateMachine):
                 return
             # this is going to be called at end of intersession after going to detection phase,
             # basically when inference is back to live
-            if not pellet_seen and algo.diamond_recently_seen and algo.star_recently_seen:
+            if not pellet_seen and algo.triangle_recently_seen:  # algo.diamond_recently_seen and algo.star_recently_seen:
+                # if triangle seen and pellet not seen: pellet not loaded ok for sure, we should see it if it was there
                 reason = "load_pellet_when_not_seen_and_retract_or_loading"
                 logit()
                 self.load_pellet()
@@ -366,7 +367,6 @@ class PelletMachine(StateMachine):
                 # either pellet is seen, or we don't know (might be not visible on cameras),
                 if cur_state == PelletState.loading and pellet_seen:
                     self._notify_pellet_loaded_ok()
-                    # algo.pellet_loaded()
                 # current state is either retract or loading (loaded),
                 # we can do a send_pellet() but ensure covered(-or-released) is as desired, *before* sending :
                 if algo.can_release_pellet():
