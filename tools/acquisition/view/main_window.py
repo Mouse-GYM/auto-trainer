6db4ba8b8e603ed967e0bc6b7142a4d26028e2b2
Grégory Starck <gregory.starck@toptal.com>
2025-12-16 15:53:17 -0500

    Fix: ensure run-action checked state correct

--- tools/acquisition/view/main_window.py
+++ tools/acquisition/view/main_window.py
@@ -116,15 +116,18 @@ class MainWindow(QMainWindow):
         self._training_plan_combo.setEnabled(stopped)
         self.edit_camera_settings_action.setEnabled(stopped)
         #
+        run_action = self.run_action
+        run_action.blockSignals(True)  # block signal to ensure we don't re-start/stop
+        run_action.setChecked(started)
+        run_action.blockSignals(False)
         if started:
             icon = qta.icon('ei.stop')
-            self.run_action.setText("Stop")
-            self.run_action.setIcon(icon)
+            run_action.setText("Stop")
+            run_action.setIcon(icon)
         else:
             icon = qta.icon('ei.play')
-            self.run_action.setChecked(False)
-            self.run_action.setText("Start")
-            self.run_action.setIcon(icon)
+            run_action.setText("Start")
+            run_action.setIcon(icon)
 
     def on_capture_start_stop(self, is_toggled):
         app_model = self._app_model
