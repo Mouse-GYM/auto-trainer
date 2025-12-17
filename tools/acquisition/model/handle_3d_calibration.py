c7d0f1e1291a07092944b85d22ef8a8d351cab00
Grégory Starck <gregory.starck@toptal.com>
2025-12-17 15:34:00 -0500

    Retry prepare secondary first

--- tools/acquisition/model/handle_3d_calibration.py
+++ tools/acquisition/model/handle_3d_calibration.py
@@ -213,30 +213,27 @@ def make_3d_calib(
         #
 
         for cam in cameras:
-            logger.info("%s: prepare capture ..", cam.name)
-            if cam.is_primary:
+            if not cam.is_primary:
+                logger.info("%s: prepare capture ..", cam.name)
                 if not cam.on_prepare_capture():
                     raise RuntimeError(f"{cam.name}.on_prepare_capture() failed")
                 cam.wait_for_capture_status(CaptureProcessStatus.RUNNING, timeout=5)
 
         for cam in cameras:
-            logger.info("%s: prepare capture ..", cam.name)
-            if not cam.is_primary:
+            if cam.is_primary:
+                logger.info("%s: prepare capture ..", cam.name)
                 if not cam.on_prepare_capture():
                     raise RuntimeError(f"{cam.name}.on_prepare_capture() failed")
 
         wait_cams_capture_status(CaptureProcessStatus.RUNNING, timeout=5)
 
-        #
-        time.sleep(0.05)
-
         for cam in cameras:
-            logger.info("%s: capture start ..", cam.name)
             if not cam.is_primary:
+                logger.info("%s: capture start ..", cam.name)
                 cam.on_capture_start()
         for cam in cameras:
-            logger.info("%s: capture start ..", cam.name)
             if cam.is_primary:
+                logger.info("%s: capture start ..", cam.name)
                 cam.on_capture_start()
 
         if record_mode == VideoRecordMode.TRIGGER:
