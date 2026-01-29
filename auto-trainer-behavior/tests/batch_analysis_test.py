369b97d5b2a38e635094cb8dbf2fedbf8117c572
Grégory Starck <gregory.starck@toptal.com>
2026-01-29 09:03:21 -0500

    Add test_exit_tunnel_while_loading + facto

--- auto-trainer-behavior/tests/batch_analysis_test.py
+++ auto-trainer-behavior/tests/batch_analysis_test.py
@@ -10,6 +10,20 @@ from top_fixtures import MockSystemMachine
 
 class TestBatchAnalysis(MockSystemMachine):
 
+    batch_start_count = 0
+    batch_len_processed = None
+
+    def batch_starting(self, batch_len):
+        self.batch_start_count += 1
+        self.batch_len_processed = batch_len
+
+    batch_end_count = 0
+    batch_failed_count = None
+
+    def batch_ending(self, failed_count):
+        self.batch_failed_count = failed_count
+        self.batch_end_count += 1
+
     @pytest.fixture(autouse=True)
     def _init_batch_analysis(self, machine):
         algo = self.algo
@@ -17,6 +31,8 @@ class TestBatchAnalysis(MockSystemMachine):
         algo.batch_session_recording_config.enabled = True
         # make test life easier:
         machine._delay_timer_consider_end_session = 0  # TODO: use some config
+        algo.batch_analysis_starting += self.batch_starting
+        algo.batch_analysis_ending += self.batch_ending
 
     @pytest.mark.parametrize("max_batch_size", [2, 5])
     def test_with_max_batch_size(self, machine, max_batch_size, caplog):
@@ -24,21 +40,6 @@ class TestBatchAnalysis(MockSystemMachine):
         pellet = self.pellet
         algo.batch_session_recording_config.maximum_batch_size = max_batch_size
         self.start_session_in_tunnel()
-
-        batch_start_count = 0
-        def batch_starting(batch_len):
-            nonlocal batch_start_count
-            assert batch_len == max_batch_size
-            batch_start_count += 1
-
-        batch_end_count = 0
-        def batch_ending(failed_count):
-            nonlocal batch_end_count
-            assert failed_count == 0
-            batch_end_count += 1
-
-        algo.batch_analysis_starting += batch_starting
-        algo.batch_analysis_ending += batch_ending
 
         for idx in range(1, max_batch_size + 1):
             algo.update_mouse_seen(True)
@@ -60,20 +61,15 @@ class TestBatchAnalysis(MockSystemMachine):
                 self.mock_pellet_ack()
         assert machine.state == SystemState.tunnel
         assert machine.intersession.state == IntersessionState.idle
-        assert batch_start_count == batch_end_count == 1
+        assert self.batch_start_count == self.batch_end_count == 1
+        assert self.batch_len_processed == max_batch_size
+        assert self.batch_failed_count == 0
 
     def test_with_batch_size_1_do_not_use_batch(self, machine, caplog):
         algo = self.algo
         pellet = self.pellet
         algo.batch_session_recording_config.maximum_batch_size = 1
         self.start_session_in_tunnel()
-
-        batch_started = False
-        def batch_starting(batch_len):
-            nonlocal batch_started
-            batch_started = True
-
-        algo.batch_analysis_starting += batch_starting
 
         with self.mock_intersession_analysis():
             algo.update_mouse_seen(True)
@@ -84,33 +80,20 @@ class TestBatchAnalysis(MockSystemMachine):
             algo.update_pellet_seen(True)
             while pellet._api_status_token is not None:
                 self.mock_pellet_ack()
+
         assert machine.state == SystemState.tunnel
-        assert not batch_started
+        assert self.batch_start_count == 0
         assert "only 1 session in batch, skipping batch" in caplog.text
         assert machine.intersession.state == IntersessionState.idle
 
     @pytest.mark.parametrize("last_trial_with_mouse", [False, True])
-    def test_with_exit_tunnel(self, machine, caplog, last_trial_with_mouse):
+    @pytest.mark.parametrize("sessions_count", [1, 3])
+    def test_with_exit_tunnel(self, machine, caplog, last_trial_with_mouse, sessions_count):
         algo = self.algo
         pellet = self.pellet
-        batch_start_count = 0
-        batch_len_processed = None
-        def batch_starting(batch_len):
-            nonlocal batch_start_count, batch_len_processed
-            batch_start_count += 1
-            batch_len_processed = batch_len
-
-        batch_end_count = 0
-        def batch_ending(failed_count):
-            nonlocal batch_end_count
-            batch_end_count += 1
-
-        algo.batch_analysis_starting += batch_starting
-        algo.batch_analysis_ending += batch_ending
 
         self.start_session_in_tunnel()
 
-        sessions_count = 2
         for idx in range(sessions_count):
             algo.update_mouse_seen(True)
             pellet.force_load_pellet()
@@ -136,5 +119,21 @@ class TestBatchAnalysis(MockSystemMachine):
 
         assert machine.state == SystemState.cage
         assert machine.intersession.state == IntersessionState.idle
-        assert batch_len_processed == expected_batch_len
-        assert batch_start_count == batch_end_count == 1
+        assert self.batch_len_processed == expected_batch_len
+        assert self.batch_start_count == self.batch_end_count == 1
+
+    def test_exit_tunnel_while_loading(self, machine, caplog):
+        algo = self.algo
+        pellet = self.pellet
+
+        self.start_session_in_tunnel()
+        algo.update_mouse_seen(True)
+        pellet.force_load_pellet()
+        # don't ack load-pellet, but make exit tunnel now
+        with self.mock_intersession_analysis():
+            with caplog.at_level(logging.DEBUG):
+                self.exit_tunnel()
+        assert self.batch_start_count == 1
+        assert self.batch_end_count == 1
+        assert self.batch_len_processed == 1
+        assert "enter_intersession: reason=exit-tunnel-with-sessions-batch-list" in caplog.text
