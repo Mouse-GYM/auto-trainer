ed2f2f063c97d3f331785bef38fe559402acdd0f
Grégory Starck <gregory.starck@toptal.com>
2026-02-05 16:27:36 -0500

    simpler reason text

--- tools/acquisition/model/behavior_model.py
+++ tools/acquisition/model/behavior_model.py
@@ -71,7 +71,8 @@ class BehaviorModel(ObservableObject, ProjectDependentProtol):
         logger.debug("alarm-mon: %s : %s -> %s", name, old_value, value)
         if name == EmergencyAlarmMonitor.IS_ENGAGED:
             if value:
-                self.emergency_stop(f"alarm-monitor: {self._analysis.emergency_alarm_monitor.engaged_reasons}")
+                reasons = " ".join(self._analysis.emergency_alarm_monitor.engaged_reasons)
+                self.emergency_stop(f"alarm-monitor: {reasons}")
             else:
                 self.emergency_resume("alarm-monitor-resumed")
 
