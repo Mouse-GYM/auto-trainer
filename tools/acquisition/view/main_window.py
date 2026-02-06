c317d706c1a56e6063da4a11bbc7e8c746aa5671
Grégory Starck <gregory.starck@toptal.com>
2026-02-06 16:00:40 -0500

    label

--- tools/acquisition/view/main_window.py
+++ tools/acquisition/view/main_window.py
@@ -874,7 +874,7 @@ class MainWindow(QMainWindow):
             label = QLabel("R:")
             label.setToolTip("Pellets Reached")
             hbox.addWidget(label)
-            spinbox = self._internal_pellet_total_reached_spinbox = QSpinBox()
+            spinbox = self._internal_pellet_total_reaches_spinbox = QSpinBox()
             spinbox.setToolTip(label.toolTip())
             hbox.addWidget(spinbox)
             label = QLabel("C:")
@@ -884,7 +884,7 @@ class MainWindow(QMainWindow):
             spinbox.setToolTip(label.toolTip())
             hbox.addWidget(spinbox)
             label = QLabel("S:")
-            label.setToolTip("Success")
+            label.setToolTip("Success Reaches")
             hbox.addWidget(label)
             spinbox = self._internal_success_reaches_spinbox = QSpinBox()
             spinbox.setToolTip(label.toolTip())
@@ -968,7 +968,7 @@ class MainWindow(QMainWindow):
             inference._feed_intersession_analysis_execute = self._simulate_intersession_segmentation
             res = IntersessionResponse(
                 pellets_presented=self._internal_pellet_presented_spinbox.value(),
-                total_reaches=self._internal_pellet_total_reached_spinbox.value(),
+                total_reaches=self._internal_pellet_total_reaches_spinbox.value(),
                 successful_reaches=self._internal_success_reaches_spinbox.value(),
                 food_consumed=self._internal_pellet_consumed_spinbox.value(),
                 pellet_x=self._internal_shift_x_spinbox.value(),
