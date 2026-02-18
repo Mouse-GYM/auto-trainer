cdb598bbfcef4d850874ee637540a094efb66bb8
Grégory Starck <gregory.starck@toptal.com>
2026-02-18 10:39:12 -0500

    Remove stop day script + better start

--- start_day_operations.sh
+++ start_day_operations.sh
@@ -4,6 +4,12 @@ set -e
 
 echo "Updating view from origin"
 git fetch origin
+echo
+
+echo "Feel free to checkout what you need. which can be:"
+git branch -r | grep origin
+echo
 
 echo "Checking out origin/develop .."
 git checkout origin/develop
+echo
--- stop_day_operations.sh
+++
@@ -1,3 +0,0 @@
-#!/usr/bin/env bash
-
-python jetson_daily_cleanup.py
