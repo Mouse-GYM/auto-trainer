ac7d1e9ad8bd07095c1e10686f894b819ac80259
Grégory Starck <gregory.starck@toptal.com>
2026-03-05 08:26:14 -0500

    clean: unused now

--- auto-trainer-core/src/autotrainer/core/analysis/external_doors_monitor.py
+++ auto-trainer-core/src/autotrainer/core/analysis/external_doors_monitor.py
@@ -4,8 +4,6 @@ import threading
 import time
 from collections import namedtuple
 from typing import Dict, Tuple, Optional, NamedTuple
-from typing_extensions import TypeAlias
-
 
 from autotrainer.api.api_event_kind import ApiDetectorKind
 from autotrainer.core import ObservableObject, get_perf_now, EventManager, ApiEventKind
@@ -20,8 +18,6 @@ class DoorState(NamedTuple):
     open: Optional[bool]
     perf_c: float  # perf_c when last state change
 
-
-DoorsStateT: TypeAlias = Dict[SystemStatusMessageKind, DoorState]
 
 FrontDoor = SystemStatusMessageKind.FRONT_DOOR
 SlidingDoor = SystemStatusMessageKind.DRAWER_DOOR
