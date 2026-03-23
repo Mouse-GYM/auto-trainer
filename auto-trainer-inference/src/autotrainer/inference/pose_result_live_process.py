7d13566cda95b17c5862a432dc07d997f768a50a
Grégory Starck <gregory.starck@toptal.com>
2026-03-23 08:54:44 -0400

    Add log + comment

--- auto-trainer-inference/src/autotrainer/inference/pose_result_live_process.py
+++ auto-trainer-inference/src/autotrainer/inference/pose_result_live_process.py
@@ -2,7 +2,8 @@
 
 import functools
 import multiprocessing
-from typing import Optional
+import queue
+from typing import Optional, Union
 
 from autotrainer.core.logging import get_verbose_logger
 from autotrainer.core.multiproc import pool_init
@@ -12,7 +13,7 @@ from autotrainer.inference import PoseAlgorithm, InferenceMonitorDataMsg
 logger = get_verbose_logger(__name__)
 
 
-_output_data_queue: Optional[multiprocessing.Queue] = None
+_output_data_queue: Optional[Union[queue.Queue, multiprocessing.Queue]] = None
 _pose_algo_process = None
 
 
@@ -20,12 +21,16 @@ def pool_init_process_pose_data(pose_algo: PoseAlgorithm, output_data_queue, mon
     pool_init(log_config)
     global _pose_algo_process, _output_data_queue
     _output_data_queue = output_data_queue
+    # interestingly the output_data_queue we get here is a `queue.Queue` ; ie a thread queue,
+    # while the one which is passed to this process pool init func is a `multiprocessing.Queue`,
+    # might be on this receiving side it's wrapped into such a thread queue proxy eventually...
     _pose_algo_process = functools.partial(pose_algo.process, pairs_3d_offsets=monitored_parts_offsets)
-    logger.success("Initialized with %s and %s", pose_algo, monitored_parts_offsets)
+    logger.success("Initialized with %s and %s ; q=%s", pose_algo, monitored_parts_offsets, output_data_queue)
 
 
 def pool_process_pose_data(pose_data):
-    # logger.debug("received workload %s", type(pose_data))
+    # logger.debug("received workload %s ; q=%s - %s", type(pose_data), q, type(q))
+    assert _output_data_queue is not None
     rsp = _pose_algo_process(pose_data)  # noqa
     # (cmd, (args, kwargs)) :
     _output_data_queue.put((InferenceMonitorDataMsg.POSE_RESULT_READY, ((rsp,), None)))
