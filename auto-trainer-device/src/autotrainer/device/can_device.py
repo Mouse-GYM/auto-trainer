6275ba21e819cab107c8619babfd1469a4bc4015
Grégory Starck <gregory.starck@toptal.com>
2026-04-09 13:44:21 -0400

    Set back default_command_ack_timeout_duration: to 3s

--- auto-trainer-device/src/autotrainer/device/can_device.py
+++ auto-trainer-device/src/autotrainer/device/can_device.py
@@ -119,8 +119,7 @@ def apply_system_command_with_data_args(func, data):
 class CanDevice(Device):
 
     default_command_write_failed_repeat_count: int = 3
-    default_command_ack_timeout_duration: float = 8  # seconds
-    # TODO: handle different timeout per command kind
+    default_command_ack_timeout_duration: float = 3  # seconds
 
     default_command_ack_timeout_repeat_count: int = 3
 
