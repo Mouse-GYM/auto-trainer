b39547ecb409903b07a82c176f56c4f95ef5e4d6
Grégory Starck <gregory.starck@toptal.com>
2026-04-17 05:23:45 -0400

    Enh/Fix: load-cell tare monitor

--- auto-trainer-core/tests/load_cell_tare_monitor_test.py
+++ auto-trainer-core/tests/load_cell_tare_monitor_test.py
@@ -2,6 +2,7 @@ import math
 
 import numpy
 import pytest
+from unittest import mock
 
 from autotrainer.core.analysis.load_cell_tare_monitor import LoadCellTareMonitor
 
@@ -25,11 +26,11 @@ def test_baseline_depends_on_cb_return(threshold, range_threshold, sample_rate, 
     # could use some variability in values too (but below range_threshold):
     values = numpy.asarray([monitor.threshold + 0.01] * int(monitor.sample_rate * monitor.duration))
     #
-    monitor.tare_callback = lambda: False
+    monitor.tare_callback = lambda force=False: False
     monitor.update(list(values))
     assert monitor.baseline == numpy.average(values)
     #
-    monitor.tare_callback = lambda: True
+    monitor.tare_callback = lambda force=False: True
     values *= 2
     values += 0.00001
     monitor.update(list(values))
@@ -62,3 +63,25 @@ def test_with_more_values_than_buffer_size():
     monitor.update(values)
     assert (monitor._values == list(range(50, 250))).all()
     assert monitor._index == 0
+
+
+def test_buffer_outside_range_with_nans():
+    monitor = LoadCellTareMonitor()
+    cb_called = False
+    def tare_cb(*, force: bool=False):
+        nonlocal cb_called
+        cb_called = True
+    monitor.tare_callback = tare_cb
+    monitor.update([math.nan] * len(monitor._values))
+    assert cb_called
+    assert monitor.context.low_variance_engaged
+
+
+def test_with_all_nans():
+    monitor = LoadCellTareMonitor()
+    values = [math.nan] * 10  # len(monitor._values)
+    monitor.tare_callback = mock.MagicMock()
+    for _ in range(20):
+        monitor.update(values)
+    assert monitor.context.low_variance_engaged
+    assert monitor.tare_callback.call_count >= 1
