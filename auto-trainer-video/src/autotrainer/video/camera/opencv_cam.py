5680c3a908c477c98e12776ab767e32638082c11
Grégory Starck <gregory.starck@toptal.com>
2026-05-04 09:26:03 -0400

    Enh: add detect frame drop with perf_counter on opencv cam (issue-772)

--- auto-trainer-video/src/autotrainer/video/camera/opencv_cam.py
+++ auto-trainer-video/src/autotrainer/video/camera/opencv_cam.py
@@ -1,4 +1,5 @@
 import logging
+import math
 import time
 from typing import Tuple, Optional
 
@@ -17,6 +18,8 @@ class OpenCVCam(CameraBase):
         self._device_idx = device_idx
         self._video_capture: Optional[cv2.VideoCapture] = None
         self._mjpeg = None
+        self._prev_frame_perf_now = -math.inf
+        self._frame_half_period = math.nan
 
     def init(self):
         vc = self._video_capture = cv2.VideoCapture()
@@ -25,7 +28,10 @@ class OpenCVCam(CameraBase):
         self._apply_settings()
         # re-read:
         self._refresh_height_width()
+        requested_fps = self._fps
         self._fps = self._video_capture.get(cv2.CAP_PROP_FPS)
+        logger.info("requested fps=%s ; obtained fps=%s", requested_fps, self._fps)
+        self._frame_half_period = 0.5 / self._fps
 
     def _apply_settings(self):
         vc = self._video_capture
@@ -76,10 +82,18 @@ class OpenCVCam(CameraBase):
         self._video_capture.release()
 
     def capture(self) -> Tuple[numpy.ndarray, int]:
+        prev_frame_id = self._last_frame_id
         super().capture()
-        ret, frame = self._video_capture.read()
-        if ret:
-            self._last_when = time.time_ns()
+        vc = self._video_capture
+        ret, frame = vc.read()
+        if not ret:
+            raise RuntimeError("failed read frame on %s", vc)
+        perf_now = time.perf_counter()
+        if prev_frame_id > -1:  # not first frame
+            estimated_drop = int((perf_now - self._prev_frame_perf_now - self._frame_half_period) / self._fps)
+            if estimated_drop > 0:
+                self._last_frame_id += estimated_drop
+        self._prev_frame_perf_now = perf_now
         return frame, self._last_when
 
     def _refresh_height_width(self):
