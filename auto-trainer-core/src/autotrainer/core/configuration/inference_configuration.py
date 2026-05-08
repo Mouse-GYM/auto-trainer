49019281365bf7ccd75deeaafe68702822bf075a
Grégory Starck <gregory.starck@toptal.com>
2026-05-08 07:47:05 -0400

    Remove unused InferenceConfiguration.intersession_wait_time + InferenceModel.intersession_wait_time
    not anymore needed since inference is now correctly synced with camera processes

--- auto-trainer-core/src/autotrainer/core/configuration/inference_configuration.py
+++ auto-trainer-core/src/autotrainer/core/configuration/inference_configuration.py
@@ -6,20 +6,24 @@ from autotrainer.core import build_kwargs_apply_mapping, make_camelize_represent
 
 
 @dataclass
-class InferenceConfiguration:
+class _InferenceConfiguration:
     pose_model_location: str = ""
     is_enabled: bool = False
-    intersession_wait_time: float = 2.0
-    """
-    The amount of time to wait for the video files to be available for processing.  This is more of a system property
-    than an algorithm/behavior property.
-    """
 
     @classmethod
     def from_version_zero(cls, content: dict) -> Self:
+        content.pop("intersession_wait_time", None)  # was deprecated/removed
         return cls(**build_kwargs_apply_mapping(content, (
             ('pose_model_location', 'model'),
         )))
+
+
+@dataclass
+class InferenceConfiguration(_InferenceConfiguration):
+
+    def __init__(self, **kwargs):
+        kwargs.pop("intersession_wait_time", None)  # was deprecated/removed
+        super().__init__(**kwargs)
 
 
 inference_configuration_representer = make_camelize_representer("!InferenceConfiguration")
