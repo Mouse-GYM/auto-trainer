9322b63d01c3e4dcf921cc94895a1da87b5a37e0
Grégory Starck <gregory.starck@toptal.com>
2026-05-09 15:34:52 -0400

    Use more conservative 5s default for WatchdogConfig.timeout_trigger_delay

--- auto-trainer-core/src/autotrainer/core/configuration/watchdog_config.py
+++ auto-trainer-core/src/autotrainer/core/configuration/watchdog_config.py
@@ -5,4 +5,5 @@ import dataclasses
 @dataclasses.dataclass
 class WatchdogConfig:
 
-    timeout_trigger_delay: float = 3
+    timeout_trigger_delay: float = 5  # seconds
+    # if watchdog perf counter older than this then trigger watchdog
