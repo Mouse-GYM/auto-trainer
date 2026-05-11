bf4d78446985ad23a9a083f52b81533be3e52a2e
Grégory Starck <gregory.starck@toptal.com>
2026-05-11 13:12:01 -0400

    type hint

--- auto-trainer-device/src/autotrainer/device/can_device.py
+++ auto-trainer-device/src/autotrainer/device/can_device.py
@@ -909,7 +909,7 @@ class CanDevice(Device):
         self._compound_movement = move_steps
         return self._perform_next_compound_step(move_steps)
 
-    def _find_step_board(self, step):
+    def _find_step_board(self, step) -> Optional[Target]:
         if 'x' in step:
             motor = Motor.PELLET_X_MOTOR
         elif 'y' in step:
