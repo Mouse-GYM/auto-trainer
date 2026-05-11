c09177f7819161e4076820c12a3dfffcbe80eeb1
Grégory Starck <gregory.starck@toptal.com>
2026-05-11 16:19:00 -0400

    Revert "Fix: must reset prev_command to None !"
    This reverts commit b28cfdbf06ad6420e641d7d3c1f38cf51af0955f.

--- auto-trainer-device/src/autotrainer/device/can_device.py
+++ auto-trainer-device/src/autotrainer/device/can_device.py
@@ -804,7 +804,6 @@ class CanDevice(Device):
                     logger.warning("Unexpected uuid change count: before=%s after=%s", before_uuid, after_uuid)
                 #
                 prev_command = self._prev_command
-                self._prev_command = None
                 if prev_command is None:  # given compound step do set it itself
                     prev_command = (_retry_full, (kind, data), ctx)
                 else:
