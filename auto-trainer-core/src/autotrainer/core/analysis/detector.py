5b701d5071a18b5270fe86d7de792c069696ef95
Grégory Starck <gregory.starck@toptal.com>
2026-09-10 15:28:18 -0400

    Don't clear engaged reasons on _start. keep previous value

--- auto-trainer-core/src/autotrainer/core/analysis/detector.py
+++ auto-trainer-core/src/autotrainer/core/analysis/detector.py
@@ -381,7 +381,6 @@ class GroupSubDetectorContext:
     property_changed_callback: Callable
 
 
-
 GroupSubDetectorT = TypeVar("GroupSubDetectorT", bound=BaseDetector[DetectorConfig])
 
 
@@ -419,7 +418,6 @@ class GroupBaseDetector(BaseDetector[DetectorConfigT], Generic[DetectorConfigT, 
 
     def _start(self):
         super()._start()
-        self._engaged_reasons.clear()
         for sub in self._sub_detectors.values():
             sub.detector.start()
 
