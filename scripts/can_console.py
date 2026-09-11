0a2ae3214845c7cba0cd27907abc5b4574653775
Grégory Starck <gregory.starck@toptal.com>
2026-09-11 08:18:51 -0400

    Handle comments

--- scripts/can_console.py
+++ scripts/can_console.py
@@ -415,7 +415,8 @@ def run_monitor():
             # 'm' - magnet servo
             # 'o' - set output
             # 'p' - pellet move commands
-            # 'r' - RGB LED
+            # 'r' - move retract
+            # 'rgb' - RGB LED
             # 's' - System status
             # 't' - Tare scales
             # 'v' - version
@@ -471,6 +472,10 @@ def run_monitor():
                     break
 
                 elif cmd == 'rgb':
+                    if len(params) != 3:
+                        logger.warning("expected R G B % values")
+                        get_input = True
+                        continue
                     device_connection.send_message(SystemCommandKind.SET_RGB_LED,
                                                (int(params[0]), int(params[1]), int(params[2])),
                                                context="rgb")
@@ -499,6 +504,7 @@ def run_monitor():
                 elif cmd == 'board_reboot':
                     if len(params) != 1:
                         logger.warning("expected 1 board target name (magnet or pellet)")
+                        get_input = True
                         continue
                     if params[0] == "magnet":
                         tgt = Target.MAGNET_DEVICE
@@ -506,6 +512,7 @@ def run_monitor():
                         tgt = Target.PELLET_DEVICE
                     else:
                         logger.error("unknown target board: %s", params[0])
+                        get_input = True
                         continue
                     device_connection.send_message(SystemCommandKind.BOARD_REBOOT, tgt, context=f"board_reboot_{tgt}")
 
@@ -717,8 +724,6 @@ def print_help():
           " ::Release Pellet Sequence")
     print("p[ellet] s[end]                    "
           " ::Send Pellet Sequence")
-    print("r[etract]                          "
-          "::Retract Pellet Sequence")
     print()
 
     print("a[udio] <freq> <period>            "
@@ -729,6 +734,8 @@ def print_help():
           " ::Go to Home Position (0, 0, 0)")
     print("k[nown]                            "
           " ::Go to Known/Send Position (X, Y, Z)")
+    print("r[etract]                          "
+          " ::Retract Pellet Sequence")
     print("f[ile] motor <file>                "
           " ::Load Motor Configuration")
     print("f[ile] move <file>                 "
